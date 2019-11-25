c8072b126abe275744c6d7eb8aa3584833f45497
Miss-Cheese <nat.galligan@gmail.com>
2019-11-25 17:55:40 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,44 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting|
+    painting.gallery == self
+    }
+  end
+
+  def artists
+    paintings.map {|painting|
+    painting.artist
+    }
+  end
+
+  def artist_names
+    artists.map {|artist|
+    artist.name
+    }
+  end
+
+  def most_expensive_painting
+    most_expensive_painting = 0
+    paintings.each {|painting|
+    if painting.price > most_expensive_painting
+      most_expensive_painting = painting.price
+    end
+    }
+    most_expensive_painting
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,20 @@
 require_relative '../config/environment.rb'
 
 
+artist1 = Artist.new("Salvador Dali", 20)
+artist2 = Artist.new("Leonardo", 50)
+artist3 = Artist.new("Andy Warhol", 10)
+
+gallery1 = Gallery.new("Met", "New York")
+gallery2 = Gallery.new("Tate", "London")
+gallery3 = Gallery.new("Tretyakovskaya Gallery", "Moscow")
+
+painting1 = Painting.new(artist2, "Mona Lisa", 750_000, gallery3)
+painting2 = Painting.new(artist1, "Black Square", 650_000, gallery2)
+painting3 = Painting.new(artist3, "Cambell Soup", 500_000, gallery1)
+painting4 = Painting.new(artist1, "Minerva", 200_000, gallery1)
+painting5 = Painting.new(artist2, "Autoportrait", 170_000, gallery2)
+painting6 = Painting.new(artist3, "Sunset", 350_000, gallery3)
 
 
 binding.pry
