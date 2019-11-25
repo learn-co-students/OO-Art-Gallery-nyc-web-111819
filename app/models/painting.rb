93246c9790fdea6c119481be50c86917629acf69
github username <github email address>
2019-11-25 17:32:44 -0500

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    self.class.all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.total_price
+    self.all.reduce(0) do |total, painting|
+      total + painting.price 
+    end 
+  end 
 
 end
